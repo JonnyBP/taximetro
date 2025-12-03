34d7a3c1d4ef8fea8d6b750ba128fe27953db991
JonnyBP <jpbrasale@gmail.com>
2025-12-03 09:40:22 +0100

    feat: Crear la bienvenida y sus opciones. issue  #2

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print ("Hola")
+import time
+
+def taximeter():
+    #Función para manejar y mostrar opciones
+
+    print("Welcome to the F5 Taximeter\n" \
+            "Available commands: 'start', 'stop', 'move', 'finish', 'exit'\n")
+
+
+taximeter()
