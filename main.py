543e2e1556c900b515ae9439664c3ec79cf1fffe
JonnyBP <jpbrasale@gmail.com>
2025-12-03 14:18:53 +0100

    feature: Lógica de tiempos para calcular el valor a pagar. issue #3

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def taximeter():
     print("Welcome to the F5 Taximeter\n" \
             "Available commands: 'start', 'stop', 'move', 'finish', 'exit'\n")
 
+    # Declaramos variables
+
     trip_active = False
+    start_time = 0
+    stopped_time = 0
+    moving_time = 0
+    state = None  # 'stopped' o 'moving'
+
 
     while True:
         command = input("> ").strip().lower()
@@ -15,8 +22,14 @@ def taximeter():
             if trip_active == True:
                 print("Error: A trip is already in progress")
                 continue
+            
+            print("Start Trip\nTrip started. Initial state: 'stopped'.")
             trip_active = True
-            print("moviendo")
+            start_time = time.time()
+            stopped_time = 0
+            moving_time = 0
+            state = 'stopped'  # Iniciamos en estado 'stopped'
+            
         
         elif command in ("stop", "move"):
             if trip_active == False:
@@ -24,12 +37,28 @@ def taximeter():
                 continue
             print("Acción")
 
+            # Calculamos los tiempos de stop y move
+            duration = time.time() - start_time  # lo ponemos a 0
+            if state == "stopped":
+                stopped_time += duration
+            else:
+                moving_time += duration     
+
+
         elif command == "finish":
             if trip_active == False:
                 print("Error: No active trip to finish")
                 continue
             trip_active = False
-            print("Recorrido terminado")
+
+            # Calculamos los tiempos de stop y move
+            duration = time.time() - start_time  # lo ponemos a 0
+            if state == "stopped":
+                stopped_time += duration
+            else:
+                moving_time += duration  
+
+            print(duration)
 
         elif command == "exit":
             print("Un gusto, vuelve pronto")
